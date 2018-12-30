4a34e5f0cbdae6175f1a58740d269d1ba496c679
Dmcdominic <dmcdominic@gmail.com>
2018-12-30 00:04:43 -0500

    Stretched the container to full width (using container-fluid instead), and tweaked necessary styling accordingly.

--- scripts/header.js
+++ scripts/header.js
@@ -1,9 +1,12 @@
 var page_header = $("#page-header");
+var header_img = $("#header-img");
 window.addEventListener("scroll", function(event) {
     var scroll = this.scrollY;
     if (scroll == 0) {
         page_header.addClass("at-top");
+        header_img.attr("src", "/images/official_no_back_white.png");
     } else {
         page_header.removeClass("at-top");
+        header_img.attr("src", "/images/official_no_back.png");
     }
 });
