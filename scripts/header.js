1d4add0bcba93e698f8e9c879c909d4ebfe4a0d4
Joseph Perrino <jperrino@andrew.cmu.edu>
2020-01-05 22:57:42 -0500

    Revert "Work made to speed up site, social media images added in"
    This reverts commit 558a8a51f34af49c9f59de4408d7f265f3430889.

--- scripts/header.js
+++ scripts/header.js
@@ -4,9 +4,9 @@ window.addEventListener("scroll", function(event) {
     var scroll = this.scrollY;
     if (scroll == 0) {
         page_header.addClass("at-top");
-        header_img.attr("src", "/images/official_no_back_white_smaller.webp");
+        header_img.attr("src", "/images/official_no_back_white.png");
     } else {
         page_header.removeClass("at-top");
-        header_img.attr("src", "/images/official_no_back_smaller.webp");
+        header_img.attr("src", "/images/official_no_back.png");
     }
 });
